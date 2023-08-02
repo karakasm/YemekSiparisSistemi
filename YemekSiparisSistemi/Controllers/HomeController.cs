414ebef45ca45d3095feb704045508b9721b7aa1
karakasm <turabkarakas@gmail.com>
2023-08-02 23:43:48 +0300

    Add project files.

---
+++ YemekSiparisSistemi/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YemekSiparisSistemi.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("Index")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+  
+            return View("~/Views/Index.cshtml");
+        }
+
+        [Route("Admin")]
+        [Route("Admin/Index")]
+        [HttpGet]
+        public IActionResult AdminIndex()
+        {
+            return View("~/Views/Admin/Index.cshtml");
+        }
+
+        [Route("Restaurant")]
+        [Route("Restaurant/Index")]
+        [HttpGet]
+        public IActionResult RestaurantIndex()
+        {
+            return View("~/Views/Restaurant/Index.cshtml");
+        }
+
+        [Route("Customer")]
+        [Route("Customer/Index")]
+        [HttpGet]
+        public IActionResult CustomerIndex()
+        {
+            return View("~/Views/Customer/Index.cshtml");
+        }
+
+
+    }
+}
