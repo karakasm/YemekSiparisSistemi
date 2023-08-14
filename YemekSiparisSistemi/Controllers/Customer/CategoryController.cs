6e936633bfe74c1631fe2dd9f11ea30150dac6c3
karakasm <turabkarakas@gmail.com>
2023-08-14 19:18:43 +0300

    Müşteri restoran listeleme ve restoran ürünleri listeleme sayfaları oluşturuldu.

---
+++ YemekSiparisSistemi/Controllers/Customer/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YemekSiparisSistemi.Models;
+
+namespace YemekSiparisSistemi.Controllers.Customer
+{
+    [Authorize(Roles = Role.IS_CUSTOMER)]
+    [Route("Customer")]
+    public class CategoryController : Controller
+    {
+        private readonly FoodOrderSystemDbContext _context;
+
+        public CategoryController(FoodOrderSystemDbContext foodOrderSystemDbContext)
+        {
+            _context = foodOrderSystemDbContext;
+        }
+
+
+        [HttpGet]
+        [Route("Category/{id}/Products")]
+
+        public async Task<IActionResult> GetAllProductsByCategory(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                return PartialView("~/Views/Shared/ProductCardPartialView.cshtml", await _context.Products.Where(p => p.CategoryId == id).ToListAsync());
+            }
+            catch (Exception /*ex*/)
+            {
+                return Json(null);
+            }
+        }
+    }
+}
---
+++ YemekSiparisSistemi/Controllers/Customer/CompanyController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YemekSiparisSistemi.Models;
+
+namespace YemekSiparisSistemi.Controllers.Customer
+{
+    [Authorize(Roles = Role.IS_CUSTOMER)]
+    [Route("Customer")]
+    public class CompanyController : Controller
+    {
+        private readonly FoodOrderSystemDbContext _context;
+        public CompanyController(FoodOrderSystemDbContext foodOrderSystemDbContext)
+        {
+            _context = foodOrderSystemDbContext;
+        }
+        [HttpGet]
+        [Route("Restaurants/{id}")]
+        public async Task<IActionResult> Index(int? id)
+        {
+            if(id == null)
+            {
+                return BadRequest();
+            }
+            ViewData["Company"] = await _context.Companies.FindAsync(id);
+            return View("~/Views/Customer/Restaurant/Index.cshtml", await _context.Categories.Where(cat => cat.CompanyId == id).ToListAsync());
+        }
+
+
+        [HttpGet]
+        [Route("Restaurants/{id}/Menus")]
+        public async Task<IActionResult> GetAllMenuProducts(int? id)
+        {
+            if(id == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                return PartialView("~/Views/Shared/MenuProductCardPartialView.cshtml", await _context.Menus.Where(m => m.CompanyId == id).ToListAsync());
+            }catch(Exception /*ex*/)
+            {
+                return Json(null);
+            }
+        }
+    }
+}
