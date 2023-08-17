717abc426d1b36660928694545b11644db76cf91
karakasm <turabkarakas@gmail.com>
2023-08-17 14:02:31 +0300

    Müşteri, menu ve ürün detay sayfaları oluşturuldu.

--- YemekSiparisSistemi/Controllers/Customer/CompanyController.cs
+++ YemekSiparisSistemi/Controllers/Customer/CompanyController.cs
@@ -44,5 +44,47 @@ namespace YemekSiparisSistemi.Controllers.Customer
                 return Json(null);
             }
         }
+
+
+        [HttpGet]
+        [Route("Restaurants/{restaurantId}/Products/{productId}")]
+
+        public async Task<IActionResult> GetProduct(int? restaurantId, int? productId)
+        {
+            if(restaurantId == null || productId == null)
+            {
+                return BadRequest();
+            }
+
+            ViewData["companyInfo"] = await _context.Companies.FindAsync(restaurantId);
+            Product? product = await _context.Products.FindAsync(productId);
+            if(product == null || ViewData["companyInfo"] == null)
+            {
+                return BadRequest();
+            }
+
+            return View("~/Views/Customer/Restaurant/Product/Index.cshtml", product);
+        }
+
+
+        [HttpGet]
+        [Route("Restaurants/{restaurantId}/Menus/{menuId}")]
+
+        public async Task<IActionResult> GetMenu(int? restaurantId, int? menuId)
+        {
+            if (restaurantId == null || menuId == null)
+            {
+                return BadRequest();
+            }
+
+            ViewData["companyInfo"] = await _context.Companies.FindAsync(restaurantId);
+            Menu? menu = await _context.Menus.Include(m=>m.MenuProducts).FirstOrDefaultAsync(m => m.Id == menuId);
+            if (menu == null || ViewData["companyInfo"] == null)
+            {
+                return BadRequest();
+            }
+
+            return View("~/Views/Customer/Restaurant/Menu/Index.cshtml", menu);
+        }
     }
 }
